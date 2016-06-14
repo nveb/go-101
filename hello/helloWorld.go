7fb852544e1711030b9df45a207a8bd42857d36f
naveen <nveb@rocketmail.com>
2016-06-14 10:56:31 +0530

    Updated to add init() method

--- hello/helloWorld.go
+++ hello/helloWorld.go
@@ -1,7 +1,12 @@
-// Every 101 experiments happen in this package
+// Executable package
 package main
 
 import "fmt"
+
+// init() - called before main()
+func init() {
+	fmt.Println("Initializing ...")
+}
 
 // main() - starting point of every executable program
 func main() {
