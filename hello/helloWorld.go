5953b0d304dfc448e1c2f26f518259e8c8ad3478
naveen <nveb@rocketmail.com>
2016-06-14 10:48:59 +0530

    Hello World program

---
+++ hello/helloWorld.go
@@ -0,0 +1,10 @@
+// Every 101 experiments happen in this package
+package main
+
+import "fmt"
+
+// main() - starting point of every executable program
+func main() {
+	// fmt.Println() - put some content on the console or standard output
+	fmt.Println("Hello World!")
+}
